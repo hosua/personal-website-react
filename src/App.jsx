c96ec724992056619f757e28037e03576da6225a
hosua <hosua@gmail.com>
2023-09-10 14:54:05 -0400

    Forgot to add in the Guardian Star chart

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,14 @@ import Portfolio from './Portfolio.jsx';
 import { YFMDuelCommonStratsApp } from './yugioh-fm-tools/src/YFMDuelCommonStrats.jsx';
 import { YFMDuelCalcApp } from './yugioh-fm-tools/src/YFMDuelCalc.jsx';
 import { YFMDatabaseCardList, YFMDatabaseCardDrops } from './yugioh-fm-tools/src/YFMDatabase.jsx';
+import { YFMGuardianStarChart } from './yugioh-fm-tools/src/YFMGuardianStars';
 
 function AppNavBar() {
     return (
         <>
             <Navbar data-bs-theme="dark" expand="lg" className="bg-body-tertiary">
                 <Container>
-                    <Navbar.Brand as={Link} to="/">Hoswoo&quots Website</Navbar.Brand>
+                    <Navbar.Brand as={Link} to="/">Hoswoo's Website</Navbar.Brand>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="me-auto">
@@ -27,6 +28,7 @@ function AppNavBar() {
                             <NavDropdown title="Yu-Gi-Oh - FM">
                                 <NavDropdown.Item as={Link} to="/yugioh-fm-tools/duel-calculator">Duel Points Calculator</NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/yugioh-fm-tools/duel-common-strats">Duel Common Strategies</NavDropdown.Item>
+                                <NavDropdown.Item as={Link} to="/yugioh-fm-tools/guardian-stars">Guardian Star Chart</NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/yugioh-fm-tools/database/card-list">Card List</NavDropdown.Item>
                                 <NavDropdown.Item as={Link} to="/yugioh-fm-tools/database/card-drops">Card Drops</NavDropdown.Item>
                             </NavDropdown>
@@ -50,6 +52,7 @@ export default function App() {
                         <Route path="/forum" element={<Forum />} />
                         <Route path="/portfolio" element={<Portfolio />} />
                         <Route path="/contact" element={<Contact />} />
+                        <Route path="/yugioh-fm-tools/guardian-stars" element={<YFMGuardianStarChart />} />
                         <Route path="/yugioh-fm-tools/duel-common-strats" element={<YFMDuelCommonStratsApp />} />
                         <Route path="/yugioh-fm-tools/duel-calculator" element={<YFMDuelCalcApp />} />
                         <Route path="/yugioh-fm-tools/database/card-list" element={<YFMDatabaseCardList />} />
--- src/Home.jsx
+++ src/Home.jsx
@@ -1,10 +1,8 @@
-import React from "react";
-import App from "./App.jsx"
 import './App.css'
 
 export default function Home() {
     return (
-        <div className="App-body">
+        <div className="App-body" style={{ textAlign: 'center' }}>
             <h1>Home Page</h1>
         </div>
     );
